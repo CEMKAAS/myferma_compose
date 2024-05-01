a325860d89e1ce913a61487dd116a4aac4b7ca6e
CEMKAAS-MASTERS <zaroslikob@mail.ru>
2024-05-02 00:01:00 +0300

    Add screen

--- app/src/main/java/com/zaroslikov/myfermacompose/ui/navigator/AddProduct.kt
+++ app/src/main/java/com/zaroslikov/myfermacompose/ui/navigator/AddProduct.kt
@@ -1,5 +1,6 @@
 package com.zaroslikov.myfermacompose.ui.navigator
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,11 +10,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -46,6 +52,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.zaroslikov.myfermacompose.ui.Incubator.MyRowIncubatorAdd
 import com.zaroslikov.myfermacompose.ui.MyAppFerma
 import com.zaroslikov.myfermacompose.ui.TopAppBar
 import kotlinx.coroutines.CoroutineScope
@@ -82,10 +89,11 @@ fun AddProduct(scope: CoroutineScope, drawerState: DrawerState) {
 @Composable
 fun AddProductContainer(modifier: Modifier, showBottom: MutableState<Boolean>) {
 
-    Column(modifier = modifier) {
-        Text(text = "sd")
+    LazyVerticalGrid(columns = GridCells.Fixed(1)) {
+        items(30) {
+           AddProductCard()
+        }
     }
-
 
     if (showBottom.value) {
         AddProductSheet(
@@ -150,70 +158,53 @@ fun AddProductSheet(
 
 
 @Composable
-fun AddProductCard(){
-
-    //clikable
-    Row(
+fun AddProductCard() {
+    Card(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(35.dp),
-        verticalAlignment = Alignment.CenterVertically
+            .padding(8.dp)
+            .clickable {
+//                navController.navigate("MyFerma")
+            },
+        elevation = CardDefaults.cardElevation(10.dp),
+        colors = CardDefaults.cardColors()
     ) {
+        //clikable
 
-        Text(
-            text = "Яйца",
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth(0.16f)
-                .padding(6.dp)
-        )
-        Divider(
-            color = Color.DarkGray,
-            modifier = Modifier
-                .fillMaxHeight()
-                .width(1.dp)
-        )
-        Text(
-            text = "33",
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth(0.16f)
-                .padding(6.dp)
-        )
-        Divider(
-            color = Color.DarkGray,
-            modifier = Modifier
-                .fillMaxHeight()
-                .width(1.dp)
-        )
-        Text(
-            text = "0",
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth(0.3f)
-                .padding(6.dp)
-        )
-        Divider(
-            color = Color.DarkGray,
-            modifier = Modifier
-                .fillMaxHeight()
-                .width(1.dp)
-        )
-        Text(
-            text = "01.05.2024",
-            textAlign = TextAlign.Center,
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(6.dp)
-        )
+                .wrapContentHeight(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
 
+            Column {
+                Text(
+                    text = "Яйца",
+                    modifier = Modifier
+                        .fillMaxWidth(0.16f)
+                        .padding(6.dp)
+                )
+
+                Text(
+                    text = "Дата добавления: 02.05.2024",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(6.dp)
+                )
+            }
+
+            Text(
+                text = "30",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth(0.3f)
+                    .padding(6.dp)
+            )
+        }
     }
-    Divider(color = Color.DarkGray, thickness = 1.dp)
-
-
 }
-
-
 
 
 @Preview(showBackground = true)
@@ -221,8 +212,6 @@ fun AddProductCard(){
 fun AddProductCardPrewie() {
     AddProductCard()
 }
-
-
 
 
 //@Preview(showBackground = true)
